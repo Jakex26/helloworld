9eb1d97443616c424260bdcadec77bc9b09b0d1c
Jakex26 <122539748+Jakex26@users.noreply.github.com>
2023-01-13 08:44:46 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,7 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showIcon(IconNames.TShirt)
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showIcon(IconNames.Pitchfork)
+})
+basic.showIcon(IconNames.SmallSquare)
