14b6ea24fc68b212894f007402086873b1a9b035
Jakex26 <122539748+Jakex26@users.noreply.github.com>
2023-01-13 08:59:12 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,7 +1,28 @@
 input.onButtonPressed(Button.A, function () {
-    basic.showIcon(IconNames.TShirt)
+    basic.showLeds(`
+        . . # . .
+        . # # # .
+        . # # # .
+        # # # # #
+        # # # # #
+        `)
 })
 input.onButtonPressed(Button.B, function () {
-    basic.showIcon(IconNames.Pitchfork)
+    for (let index = 0; index < 4; index++) {
+        basic.showLeds(`
+            # . # . #
+            . # . # .
+            # . # . #
+            . # . # .
+            # . # . #
+            `)
+        basic.showLeds(`
+            . # . # .
+            # . # . #
+            . # . # .
+            # . # . #
+            . # . # .
+            `)
+    }
 })
 basic.showIcon(IconNames.SmallSquare)
